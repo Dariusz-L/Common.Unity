e456912a2cdc7ac54d6549cee0dcee4c38561cf6
dariusz.lusnia <dariusz.lusnia@gmail.com>
2022-02-14 09:56:13 +0100

    Add TypedText prefab script

---
+++ Prefabs/TypedText.cs
@@ -0,0 +1,46 @@
+using Common;
+using Common.Domain.Functional;
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Autonomous
+{
+    public class TypedText : MonoBehaviour
+    {
+        [SerializeField] private Text _textComponent;
+        [SerializeField] private string _text;
+        [SerializeField] private float _typeDurationSeconds;
+        [SerializeField] private bool _looped = true;
+
+        private Coroutine _coroutine;
+        private int _currentTextIndex;
+
+        public void StartTyping()
+        {
+            if (!_coroutine.IsNull())
+                StopCoroutine(_coroutine);
+
+            _currentTextIndex = 1;
+            _coroutine = GetTypeTextAction()
+                .RunAsCoroutineRepeated(() => _typeDurationSeconds, StartCoroutine);
+        }
+
+        private Action GetTypeTextAction() =>
+            () =>
+            {
+                _textComponent.text = _text.Substring(0, _currentTextIndex);
+                _currentTextIndex++;
+                if (_currentTextIndex >= _text.Length - 1)
+                {
+                    if (_looped)
+                        _currentTextIndex = 1;
+                    else
+                    {
+                        StopCoroutine(_coroutine);
+                        _coroutine = null;
+                    }
+                }
+            };
+    }
+}
