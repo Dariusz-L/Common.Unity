8653fb6d46fc5e4de8fae294fd032a24c2244435
dariusz.lusnia <dariusz.lusnia@gmail.com>
2022-02-16 14:47:29 +0100

    Add TextImage prefab script, for game dialogues

---
+++ Prefabs/TextImagePanel.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Autonomous
+{
+    public class TextImagePanel : MonoBehaviour
+    {
+        [SerializeField] private RectTransform _imageRT;
+        [SerializeField] private RectTransform _textRT;
+
+        [SerializeField] private Image _image;
+        [SerializeField] private Text _text;
+
+        public void ImageToLeft()
+        {
+            _imageRT.anchorMin = new Vector2(0, 0);
+            _imageRT.anchorMax = new Vector2(0, 0);
+            _imageRT.pivot = new Vector2(0, 0);
+            _imageRT.anchoredPosition = new Vector2(0, 0);
+
+            _textRT.anchorMin = new Vector2(1, 0);
+            _textRT.anchorMax = new Vector2(1, 1);
+            _textRT.pivot = new Vector2(1, 0.5f);
+            _textRT.anchoredPosition = new Vector2(0, 0);
+        }
+
+        public void ImageToRight()
+        {
+            _imageRT.anchorMin = new Vector2(1, 0);
+            _imageRT.anchorMax = new Vector2(1, 0);
+            _imageRT.pivot = new Vector2(1, 0);
+            _imageRT.anchoredPosition = new Vector2(0, 0);
+
+            _textRT.anchorMin = new Vector2(0, 0);
+            _textRT.anchorMax = new Vector2(0, 1);
+            _textRT.pivot = new Vector2(0, 0.5f);
+            _textRT.anchoredPosition = new Vector2(0, 0);
+        }
+    }
+}
