4a296279da8c8aa29ba8353da378f090c55f3576
dariusz.lusnia <dariusz.lusnia@gmail.com>
2022-02-17 09:45:04 +0100

    Add extensions for Graphic class

---
+++ Components/GraphicExtensions.cs
@@ -0,0 +1,36 @@
+﻿using Common.Infrastructure.Unity.GameObjects;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Common.Unity.Components
+{
+    public static class GraphicExtensions
+    {
+        public static void NestedGraphicsToClear(this GameObject gameObject)
+        {
+            gameObject.ForEachNested<Graphic>(g => g.ToClearSingle());
+        }
+
+        public static void NestedGraphicsToTransparent(this GameObject gameObject)
+        {
+            gameObject.ForEachNested<Graphic>(g => g.ToTransparentSingle());
+        }
+
+        public static void ToTransparentSingle(this Graphic graphic)
+        {
+            graphic.ModifyChannel(3, 0);
+        }
+
+        public static void ToClearSingle(this Graphic graphic)
+        {
+            graphic.ModifyChannel(3, 1);
+        }
+
+        public static void ModifyChannel(this Graphic graphic, int index, float value)
+        {
+            var color = graphic.color;
+            color[index] = value;
+            graphic.color = color;
+        }
+    }
+}
