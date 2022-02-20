cb0e9557e4b51097e64ad4c1012f389e7857ec0d
daretsuki <dariusz.lusnia@gmail.com>
2022-02-20 17:04:28 +0100

    Add script to destroy game object

---
+++ Scripts/Common/Commands/Destroy.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using static MLU.Commands.SerializableCallbacks;
+
+namespace MLU.Commands
+{
+    public class Destroy : MonoBehaviour
+    {
+        [SerializeField] private GetGameObject _getTargetGO;
+        [SerializeField] private GameObject _target;
+
+        public void Execute()
+        {
+            if (!_target)
+                _target = _getTargetGO?.Invoke();
+
+            Destroy(_target);
+        }
+    }
+};
