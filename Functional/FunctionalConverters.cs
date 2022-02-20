b0d2a4a781213196dd2424364f622e7b7d02dd1f
daretsuki <dariusz.lusnia@gmail.com>
2022-02-20 16:34:30 +0100

    Add set parent script

--- Functional/FunctionalConverters.cs
+++ Functional/FunctionalConverters.cs
@@ -7,6 +7,6 @@ namespace Common.Unity.Functional
     public static class FunctionalConverters
     {
         public static Action ToAction(this List<UnityEvent> actions) =>
-            () => actions.ForEach(h => h.Invoke());
+            () => actions.ForEach(h => h?.Invoke());
     }
 }
---
+++ Scripts/Common/Commands/SetParent.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using static MLU.Commands.SerializableCallbacks;
+
+namespace MLU.Commands
+{
+    public class SetParent : MonoBehaviour
+    {
+        [SerializeField] private GetGameObject _getGO;
+        [SerializeField] private GameObject _gameObject;
+
+        [SerializeField] private GetGameObject _getParentToSet;
+        [SerializeField] private GameObject _parentToSet;
+
+        public void Execute()
+        {
+            if (!_gameObject)
+                _gameObject = _getGO?.Invoke();
+
+            if (!_parentToSet)
+                _parentToSet = _getParentToSet?.Invoke();
+
+            if (!_parentToSet)
+                _gameObject.transform.parent = null;
+            else
+                _gameObject.transform.parent = _parentToSet.transform;
+        }
+    }
+}
