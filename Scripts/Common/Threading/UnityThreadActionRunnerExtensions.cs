c99267109456cbbd4c42e8c436e7a7944abc817a
dariusz.lusnia <dariusz.lusnia@gmail.com>
2022-03-16 23:09:46 +0100

    Add extension to cal unity event on unity thread

---
+++ Scripts/Common/Threading/UnityThreadActionRunnerExtensions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using UnityEngine.Events;
+
+namespace Common.Unity.Scripts.Common
+{
+    public static class UnityThreadActionRunnerExtensions
+    {
+        public static Action<T1, T2> RunOnUnityThread_Action<T1, T2>(this UnityEvent<T1, T2> action)
+        {
+            return (arg1, arg2) => UnityThreadActionRunner.Run(() => action.Invoke(arg1, arg2));
+        }
+    }
+}
