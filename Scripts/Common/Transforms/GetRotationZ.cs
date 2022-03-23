c8a53be6e9cdecfc7734f7c90ff7bd67edf740a8
dariusz.lusnia <dariusz.lusnia@gmail.com>
2022-03-23 18:54:07 +0100

    Create script to set and get rotation Z

---
+++ Scripts/Common/Transforms/GetRotationZ.cs
@@ -0,0 +1,19 @@
+﻿using Common.Unity.Events;
+using UnityEngine;
+
+namespace Common.Unity.Scripts.Common
+{
+    public class GetRotationZ : MonoBehaviour
+    {
+        [SerializeField] private GetGameObject _getTargetGO;
+        [SerializeField] private Transform _target;
+        
+        public float Get()
+        {
+            if (!_target)
+                _target = _getTargetGO?.Invoke().transform;
+
+            return _target.rotation.eulerAngles.z;
+        }
+    }
+}
---
+++ Scripts/Common/Transforms/SetRotationZ.cs
@@ -0,0 +1,21 @@
+﻿using Common.Unity.Components;
+using Common.Unity.Events;
+using UnityEngine;
+
+namespace Common.Unity.Scripts.Common
+{
+    public class SetRotationZ : MonoBehaviour
+    {
+        [SerializeField] private GetGameObject _getTargetGO;
+        [SerializeField] private Transform _target;
+        [SerializeField] private float _value;
+        
+        public void Execute()
+        {
+            if (!_target)
+                _target = _getTargetGO?.Invoke().transform;
+
+            _target.SetRotationZ(_value);
+        }
+    }
+}
