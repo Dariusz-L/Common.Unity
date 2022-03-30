14d5e42f4e6a921752c57b15bf0684160f625a18
dariusz.lusnia <dariusz.lusnia@gmail.com>
2022-03-30 16:48:35 +0200

    Add IO for TMP_InputField

--- Instantiation/UnityIOs.cs
+++ Instantiation/UnityIOs.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using TMPro;
 using UnityEngine;
 
 namespace Common.Unity.Instantiation
@@ -7,5 +8,6 @@ namespace Common.Unity.Instantiation
     {
         [Serializable] public class TransformIO : InstantiationObject<Transform>, IInstantiationObject<Transform> {}
         [Serializable] public class TransformIOList : InstantiationObjectList<Transform>, IInstantiationObjectList<Transform> {}
+        [Serializable] public class InputFieldTmpIO : InstantiationObject<TMP_InputField>, IInstantiationObject<TMP_InputField> { }
     }
 }
