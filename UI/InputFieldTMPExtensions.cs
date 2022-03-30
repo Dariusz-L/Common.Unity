6a92257f2d17b198bbba9f7b136c8116351e780b
dariusz.lusnia <dariusz.lusnia@gmail.com>
2022-03-31 01:53:36 +0200

    Add some extensions to create/destroy TMP_InputField

---
+++ UI/InputFieldTMPExtensions.cs
@@ -0,0 +1,40 @@
+﻿using Common.Basic.Collections;
+using System;
+using TMPro;
+
+namespace Common.Unity.UI
+{
+    public static class InputFieldTMPExtensions
+    {
+        public static void SpawnInputField(
+            Func<TMP_InputField> createInputField,
+            Action trashInputField,
+            TMP_Text modifiedText,
+            Action<string> onSubmit)
+        {
+            modifiedText.enabled = false;
+
+            var inputField = createInputField();
+            inputField.text = modifiedText.text;
+            inputField.onFocusSelectAll = true;
+            inputField.ActivateInputField();
+
+            inputField.onDeselect.AddListener(text => TrashInputFieldAndEnableText(trashInputField, modifiedText));
+            inputField.onSubmit.AddListener(text =>
+            {
+                if (!inputField.text.IsEmpty() && text != modifiedText.text)
+                    onSubmit(text);
+
+                TrashInputFieldAndEnableText(trashInputField, modifiedText);
+            });
+        }
+
+        public static void TrashInputFieldAndEnableText(
+            Action trashInputFieldBase,
+            TMP_Text modifiedText)
+        {
+            trashInputFieldBase();
+            modifiedText.enabled = true;
+        }
+    }
+}
