8f1079914ee6a03fb4c3dcb106a876cc5d53ac0c
dariusz.lusnia <dariusz.lusnia@gmail.com>
2022-04-04 22:19:14 +0200

    Add some extensions for ui to instantiate

---
+++ UI/UIInstantiateExtensions.cs
@@ -0,0 +1,25 @@
+﻿using Common.Basic.UMVC.Elements;
+using Common.Unity.Components;
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Common.Unity.UI
+{
+    public static class UIInstantiateExtensions
+    {
+        public static IButtonView InstantiateAndSetSprite(Button prefab, IView parent, Sprite sprite)
+        {
+            var button = GameObject.Instantiate(prefab, parent.ToTransform());
+            button.SetSprite(sprite);
+
+            return button.GetComponent<IButtonView>();
+        }
+
+        public static void Instantiate(Button prefab, IView parent, Action<IButtonView> onDone)
+        {
+            var button = GameObject.Instantiate(prefab, parent.ToTransform());
+            onDone(button.GetComponent<IButtonView>());
+        }
+    }
+}
