c11d1f859e884dc13fd24b35a5fac1f2bae0dc76
dariusz.lusnia <dariusz.lusnia@gmail.com>
2022-04-07 16:53:26 +0200

    Adjust setting nested interactable

--- UI/SelectableExtensions.cs
+++ UI/SelectableExtensions.cs
@@ -8,9 +8,22 @@ namespace Common.Unity.UI
 {
     public static class SelectableExtensions
     {
-        public static void SetInteractable(this IEnumerable<object> objectsWithSelectables, bool value)
+        public static void SetNestedInteractableExcept(this IEnumerable<object> objectsWithNestedSelectables, object exceptNestedSelectable, bool value)
         {
-            var items = objectsWithSelectables.GetThisAndNestedChildren<Selectable>().ToList();
+            var exceptSelectables = exceptNestedSelectable.GetThisAndNestedChildren<Selectable>().ToList();
+            objectsWithNestedSelectables.SetNestedInteractableExcept(exceptSelectables, value);
+        }
+
+        public static void SetNestedInteractableExcept(this IEnumerable<object> objectsWithNestedSelectables, IEnumerable<Selectable> exceptSelectables, bool value)
+        {
+            var items = objectsWithNestedSelectables.GetThisAndNestedChildren<Selectable>().ToList();
+            var exceptItems = items.Except(exceptSelectables);
+            exceptItems.SetInteractable(value);
+        }
+
+        public static void SetNestedInteractable(this IEnumerable<object> objectsWithNestedSelectables, bool value)
+        {
+            var items = objectsWithNestedSelectables.GetThisAndNestedChildren<Selectable>().ToList();
             items.SetInteractable(value);
         }
 
