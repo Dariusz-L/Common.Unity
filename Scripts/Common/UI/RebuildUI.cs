fcd1157e23b9b964d966928652d01eb87b94318e
dariusz.lusnia <dariusz.lusnia@gmail.com>
2022-06-09 00:04:43 +0200

    Extend rebuild ui script

--- Scripts/Common/UI/RebuildUI.cs
+++ Scripts/Common/UI/RebuildUI.cs
@@ -1,6 +1,7 @@
 ﻿using Common.Unity.Events;
 using Common.Unity.UI;
 using UnityEngine;
+using UnityEngine.Events;
 
 namespace Common.Unity.Scripts.Common
 {
@@ -9,12 +10,15 @@ namespace Common.Unity.Scripts.Common
         [SerializeField] private GetGameObject _getGameObject;
         [SerializeField] private GameObject _gameObject;
 
+        public UnityEvent OnDone;
+
         public void Execute()
         {
             if (!_gameObject)
                 _gameObject = _getGameObject?.Invoke();
 
             UILayoutRebuilder.Rebuild(_gameObject);
+            OnDone?.Invoke();
         }
     }
 }
