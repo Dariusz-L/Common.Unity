e4596c40e99221cbf2a4a282d8a0af8d117e6046
dariusz.lusnia <dariusz.lusnia@gmail.com>
2022-07-21 17:59:51 +0200

    Add extension to get tmp input field text

---
+++ Components/TMPInputFieldExtensions.cs
@@ -0,0 +1,15 @@
+﻿using Common.Unity.GameObjects;
+using TMPro;
+using UnityEngine;
+
+namespace Common.Unity.Components
+{
+    public static class TMPInputFieldExtensions
+    {
+        public static string GetTMPInputFieldText(this Component component)
+        {
+            var inputField = component.Get<TMP_InputField>();
+            return inputField.text;
+        }
+    }
+}
